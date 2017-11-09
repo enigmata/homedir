b06c6f67593dfeb73e92bda0fb76f107d01b8f65
Randy Horman <horman@ca.ibm.com>
2017-11-09 10:59:56 -0500

    Add support for fzf fuzzy finder

--- .bashrc
+++ .bashrc
@@ -96,3 +96,12 @@ fi
 
 # added by travis gem
 [ -f ~/.travis/travis.sh ] && source ~/.travis/travis.sh
+
+# fuzzy finder
+if [ -f ~/.fzf.bash ]
+then
+    source ~/.fzf.bash
+    #export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+    #export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+    bind -x '"\C-p": vim $(fzf);'
+fi
---
+++ .fzf.bash
@@ -0,0 +1,16 @@
+# Setup fzf
+# ---------
+
+# Will assume fzf is already in the path
+# if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+#   export PATH="$PATH:/usr/local/opt/fzf/bin"
+# fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.bash"
+
