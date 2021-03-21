4d216fc039694d14ea29af396ee854d9443c3856
Randy Horman <rwhorman@gmail.com>
2021-03-21 17:12:57 -0400

    include updating my key python packages

--- mbin/update-tools.py
+++ mbin/update-tools.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
+import os
 import subprocess
 
 print('\nUPDATE: Submodules...\n')
@@ -7,6 +9,10 @@ subprocess.run(["git-submodule-update.py"])
 
 print('\nUPDATE: Rust ...\n')
 subprocess.run(["rustup", "update"])
+
+requirements_file=Path(os.environ["HOME"])/".config"/"python"/"requirements.txt"
+print(f'\nUPDATE: Python packages ({requirements_file})...\n')
+subprocess.run(["python3", "-m", "pip", "install", "-U", "-r", requirements_file])
 
 print('\nUPDATE: Homebrew ...\n')
 subprocess.run(["brew", "upgrade"])
