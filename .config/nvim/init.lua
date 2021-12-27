43cc392e3fd406f897827c92d6ae82ca4c060c46
Randy Horman <rwhorman@gmail.com>
2021-12-27 15:19:27 -0500

    start translation of .vimrc to init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,18 @@
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.ruler = true
+vim.opt.hidden = true
+vim.opt.autoindent = true
+vim.opt.signcolumn = 'auto'
+vim.opt.wrap = false
+vim.opt.wildmenu = true
+vim.opt.confirm = true
+vim.opt.scrolloff = 8
+vim.opt.laststatus = 2
+vim.opt.cpoptions = '$'
+vim.opt.ts = 4 
+vim.opt.sts = 4
+vim.opt.sw = 4
+vim.opt.expandtab = true
+
