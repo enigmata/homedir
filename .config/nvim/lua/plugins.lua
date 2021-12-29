fd0fb010423bce38c36dfa68e46e4ef3e5c296bc
Randy Horman <rwhorman@gmail.com>
2021-12-29 14:20:22 -0500

    automatically PackerCompile new or changed plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,11 @@
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 end)
