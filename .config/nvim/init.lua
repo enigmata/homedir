54c992c7b543be32a20f8f9349bf7dc22cd70b79
Randy Horman <rwhorman@gmail.com>
2021-12-30 09:45:20 -0500

    basic, starting treesitter config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,27 +1,26 @@
-
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.opt.ruler = true
-vim.opt.hidden = true
-vim.opt.autoindent = true
-vim.opt.signcolumn = 'auto'
-vim.opt.wrap = false
-vim.opt.wildmenu = true
-vim.opt.confirm = true
-vim.opt.scrolloff = 8
-vim.opt.laststatus = 2
-vim.opt.cpoptions = '$'
-vim.opt.ts = 4 
-vim.opt.sts = 4
-vim.opt.sw = 4
-vim.opt.expandtab = true
-vim.opt.listchars = {tab = '▸ ', eol = '¬', trail = '·'}
-vim.opt.background = 'dark'
-vim.opt.syntax = 'enable'
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
+vim.opt.relativenumber=true
+vim.opt.number=true
+vim.opt.ruler=true
+vim.opt.hidden=true
+vim.opt.autoindent=true
+vim.opt.signcolumn='auto'
+vim.opt.wrap=false
+vim.opt.wildmenu=true
+vim.opt.confirm=true
+vim.opt.scrolloff=8
+vim.opt.laststatus=2
+vim.opt.cpoptions='$'
+vim.opt.ts=4 
+vim.opt.sts=4
+vim.opt.sw=4
+vim.opt.expandtab=true
+vim.opt.listchars={tab = '▸ ', eol = '¬', trail = '·'}
+vim.opt.background='dark'
+vim.opt.syntax='enable'
+vim.opt.incsearch=true
+vim.opt.ignorecase=true
+vim.opt.smartcase=true
+vim.opt.hlsearch=true
 
 vim.cmd('colorscheme solarized')
 
