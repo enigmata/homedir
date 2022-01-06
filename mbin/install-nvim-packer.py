61f1c312541f7a9ad375058c256e7adbf172ffae
Randy Horman <rwhorman@gmail.com>
2022-01-06 14:10:54 -0500

    port to win10

---
+++ mbin/install-nvim-packer.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+import shutil
+import subprocess
+import sys
+
+starting_dir=Path.cwd()
+home_dir=Path.home()
+packer_dir=home_dir / Path(".local/share/nvim/site/pack/packer/start/packer.nvim")
+
+if not Path.exists(packer_dir):
+    subprocess.run(["git","clone","--depth","1","https://github.com/wbthomason/packer.nvim",packer_dir])
+
+sys.exit(0)
