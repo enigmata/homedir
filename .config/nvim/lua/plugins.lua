483dd9fc291c3f697caef32a00b1729e6cd02b63
Randy Horman <rwhorman@gmail.com>
2022-01-17 11:21:32 -0500

    take a dive into the treesitter text obj pool

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -23,6 +23,7 @@ return packer.startup(function()
   use 'wbthomason/packer.nvim'
   use 'EdenEast/nightfox.nvim'
   use {'nvim-treesitter/nvim-treesitter', run=':TSUpdate'}
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
   use 'kyazdani42/nvim-tree.lua'
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp'
